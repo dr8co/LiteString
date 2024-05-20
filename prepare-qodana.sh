e88905b61a44e34ff0fc07d48b0c7788777dfad6
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2024-05-20 03:10:26 +0300

    ci: update Qodana configuration

---
+++ prepare-qodana.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Run from this directory
+cd "$(dirname "$0")" || (echo "Running from $(pwd)" && exit 1)
+
+# Install dependencies
+sudo apt update && sudo apt install -y ninja-build clang build-essential
+
+# Download and install CMake
+wget -qO- "https://github.com/Kitware/CMake/releases/download/v3.29.3/cmake-3.29.3-linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
+
+# Configure CMake
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
